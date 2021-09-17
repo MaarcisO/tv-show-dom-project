264a7ac0d1598ade72250cf25b3b82ec4faa0b22
Maarcis <markbodonnell@gmail.com>
2021-09-17 23:47:50 +0100

    completed Level 100

--- script.js
+++ script.js
@@ -1,12 +1,89 @@
 //You can edit ALL of the code here
+
+const rootElem = document.getElementById("root");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+
+  let searchEpisodesBox = document.querySelector("#searchEpisodes");
+  searchEpisodesBox.addEventListener("keyup", searchEpisodes);
+}
+
+function searchEpisodes(){
+  let searchEpisodesBox = document.querySelector("#searchEpisodes");
+  console.log(searchEpisodesBox.value);
+
+  const allEpisodes = getAllEpisodes();
+  let filteredEpisodes = allEpisodes.filter(filterEpisodes);
+  makePageForEpisodes(filteredEpisodes);
+}
+
+function filterEpisodes(episode){
+  let searchEpisodesBox = document.querySelector("#searchEpisodes");
+  console.log(searchEpisodesBox.value);
+
+  rootElem.innerHTML = "";
+
+  if(episode.name.toLowerCase().includes(searchEpisodesBox.value.toLowerCase())){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach(createCard);
+}
+
+function createCard(episode){
+  let cardSpan = document.createElement("span");
+  cardSpan.id = "card-container";
+  let cardTitle = document.createElement("h2");
+  cardTitle.innerText = episode.name;
+  cardSpan.appendChild(cardTitle);
+
+/*--------------------------------------------------------------*/
+/*adds a 0 before the number if it is a single digit number*/
+  let seasonPadding = "";
+  if(episode.season < 10){
+    seasonPadding = "0";
+  }else {
+    seasonPadding = "0";
+  }
+
+  let episodePadding = "";
+  if(episode.number < 10){
+    episodePadding = "0";
+  }else {
+    episodePadding = "";
+  }
+
+  let seasonNumber = "S" + seasonPadding;
+  let episodeNumber = "E" + episodePadding;
+/*--------------------------------------------------------------*/
+
+  let cardNumber = document.createElement("p");
+  cardNumber.innerText = seasonNumber + episode.season + episodeNumber + episode.number;
+  cardSpan.appendChild(cardNumber);
+
+  let cardImage = document.createElement("img");
+  cardImage.src = episode.image.medium;
+  cardImage.alt = episode.name;
+  cardImage.title = episode.name;
+  cardImage.className = "imgCenter";
+  cardSpan.appendChild(cardImage);
+
+  let cardEpisodeSummary = document.createElement("span");
+  cardEpisodeSummary.innerHTML = episode.summary;
+  cardSpan.appendChild(cardEpisodeSummary);
+
+  cardSpan.className = "episodeCard";
+  rootElem.appendChild(cardSpan);
+  
 }
 
 window.onload = setup;
